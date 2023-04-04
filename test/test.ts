55850882c24d54c6e09acf9885957f0580f24138
minwooKang <rkdalsdn94@gmail.com>
2023-04-05 01:10:34 +0900

    test: wrap 함수 테스트 추가
    wrap 함수의 인자 s의 길이가 width보다 크면 띄어쓰기를 '\n'으로 replace 작으면 문자열 s 그대로 반환

--- test/test.ts
+++ test/test.ts
@@ -2,4 +2,13 @@ describe('Word Wrapper 테스트', () => {
     test('nothing test', () => {
         expect(undefined).toBe(undefined);
     })
+
+    test('Word Wrapper 테스트', () => {
+        expect(wrap('word word', 4)).toBe('word\nword');
+        expect(wrap('a dog', 5)).toBe('a dog');
+    })
+
+    function wrap(s: string, width: number) {
+        return s.length > width ? s.replace(' ', '\n') : s;
+    }
 })
