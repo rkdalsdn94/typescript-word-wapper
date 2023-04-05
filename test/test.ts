8b676169a7d4fede6ee39f3aab335e9e71ad2c23
minwooKang <rkdalsdn94@gmail.com>
2023-04-05 20:30:26 +0900

    test: Word Wrapper 최종 코드
    null부터 시작해 긴 문장까지 Word Wrapper 최종 코드

--- test/test.ts
+++ test/test.ts
@@ -8,18 +8,28 @@ describe('Word Wrapper 테스트', () => {
         assertWraps('', 1, '')
         assertWraps('x', 1, 'x')
         assertWraps('xx', 1, 'x\nx')
+        assertWraps('xxx', 1, 'x\nx\nx')
+        assertWraps('x x', 1, 'x\nx')
+        assertWraps('x xx', 3, 'x\nxx')
+        assertWraps('four score and seven years ago our fathers brought forth upon this continent', 7,
+            "four\nscore\nand\nseven\nyears\nago our\nfathers\nbrought\nforth\nupon\nthis\ncontine\nnt");
+
     })
 
     function assertWraps(s: string | null, width: number, expected: string) {
         expect(wrap(s, width)).toBe(expected);
     }
 
-    function wrap(s: string | null, width: number) {
+    function wrap(s: string | null, width: number): string {
         if (s == null)
             return ""
         if (s.length <= width)
             return s;
-        else
-            return s.substring(0, width) + '\n' + s.substring(width);
+        else {
+            let breakPint = s.lastIndexOf(' ', width);
+            if (breakPint == -1)
+                breakPint = width;
+            return s.substring(0, breakPint) + '\n' + wrap(s.substring(breakPint).trim(), width);
+        }
     }
 })
